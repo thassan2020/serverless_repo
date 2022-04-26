7c3316a088b2f76e9c3afab964df77c40a4c4b41
Tarek Hassan <68914484+thassan2020@users.noreply.github.com>
2022-04-26 15:47:05 -0400

    j

--- serverless-data-stream/handler.py
+++ serverless-data-stream/handler.py
@@ -1,9 +1,9 @@
 import json
-def lambda_handler(event, context):
-    import boto3
+import boto3
+def transform(event, context):
     client = boto3.client('iot')
     #Pass iteratively the list of clientids and the out put should be key value pair of clientid and device
-    response = client.describe_thing(thingName='Mythingshadow1')
+    response = client.describe_thing(thingName='myThing518')
     print(response)
     print("Thing1 response")
     print(response['attributes'])
