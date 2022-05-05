6b166b20b7d2369995570e9b9e2a62ea7c6942c9
Tarek Hassan <68914484+thassan2020@users.noreply.github.com>
2022-05-05 17:07:22 -0400

    hi

--- serverless-data-stream/handler.js
+++ serverless-data-stream/handler.js
@@ -1,8 +1,8 @@
 'use strict'
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk'); //utlizes the AWS SDK kit
 var iot = new AWS.Iot();
 
-async function getThingAttributes(thingName) {
+async function getThingAttributes(thingName) {  //Uses the getThing Attributes API function in order to match our thing or devices to the predefined attributes we have set for each device 
     try {
         const params = {
             thingName: thingName
@@ -20,21 +20,21 @@ module.exports.transform = async(event) => {
 let records = [];
 console.log("TESTING THIS")
 //console.log(event.records.data)
-for (var data of event.records) {
+for (var data of event.records) { //for loop begins for all the evengt records
     //console.log(data.data)
-    var something = data.data
+    var something = data.data //setting our data to a variable 
     //let payload = new Buffer(event.records.data, 'base64').tostring('asci');
-    var payload = Buffer.from(something, 'base64').toString();
-    payload = JSON.parse([payload]);
+    var payload = Buffer.from(something, 'base64').toString(); //setting our varaible payload to buffer from the data a
+    payload = JSON.parse([payload]); //from getting the payload we then parse the data
     //payload = SELECT topic(3) as clientid FROM '$aws/things/+/shadow/name/+/update/+
-    const clientid = payload['clientid']
+    const clientid = payload['clientid'] //the client id reads in the entire payload
     //console.log(clientid)
-    const attributes = await getThingAttributes(clientid)
+    const attributes = await getThingAttributes(clientid) //the api call from lambda function then matches the client id or device with the associated attributes 
     //console.log(attributes)
     payload['attributes'] = attributes
     console.log(payload)
     //console.log((JSON.stringify(payload)).toString('base64'))
-    payload.decoded = true;
+    payload.decoded = true; //function pushes all record ID data continously for every new data coming in from Kinesis) 
             records.push({
                 recordId: data.recordId,
                 result: 'Ok',
